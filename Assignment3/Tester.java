8c0e1c94054eb2d6cb43745b0d3153cea7d89e74
Arpit Saxena <arpit.saxena2000@yahoo.in>
2019-09-08 16:41:11 +0530

    Test code for BST

--- Assignment3/Tester.java
+++ Assignment3/Tester.java
@@ -3,12 +3,40 @@ public class Tester {
         /* System.out.println(HashFunctions.djb2("KalluYadav", 5));
         System.out.println(HashFunctions.djb2("TilluYadav", 5));
         System.out.println(HashFunctions.djb2("ShyaamSingh", 5)); */
-        System.out.println(HashFunctions.djb2("JanardanBhartiya", 5));
+        
+        /* System.out.println(HashFunctions.djb2("JanardanBhartiya", 5));
         System.out.println(HashFunctions.djb2("ShoukatIsmaail", 5));
         System.out.println(HashFunctions.djb2("JackNicholson", 5));
         System.out.println(HashFunctions.djb2("KalluYadav", 5));
         System.out.println(HashFunctions.djb2("TunniBai", 5));
-        System.out.println(HashFunctions.djb2("TilluYadav", 5));
-
+        System.out.println(HashFunctions.djb2("TilluYadav", 5)); */
+        
+        BinarySearchTree<Integer, Integer> bst = new BinarySearchTree<>();
+        System.out.println(bst.insert(8, 8));
+        System.out.println(bst.insert(6, 6));
+        System.out.println(bst.insert(10, 10));
+        System.out.println(bst.insert(2, 2));
+        System.out.println(bst.insert(7, 7));
+        System.out.println(bst.insert(9, 9));
+        System.out.println(bst.insert(20, 20));
+        System.out.println(bst.insert(1, 1));
+        System.out.println(bst.insert(4, 4));
+        System.out.println(bst.insert(5, 5));
+        System.out.println(bst.insert(15, 15));
+        System.out.println(bst.insert(17, 17));
+        System.out.println();
+        System.out.println(bst.delete(10));
+        System.out.println();
+        System.out.println(bst.address(1));
+        System.out.println(bst.address(2));
+        System.out.println(bst.address(4));
+        System.out.println(bst.address(5));
+        System.out.println(bst.address(6));
+        System.out.println(bst.address(7));
+        System.out.println(bst.address(8));
+        System.out.println(bst.address(9));
+        System.out.println(bst.address(15));
+        System.out.println(bst.address(17));
+        System.out.println(bst.address(20));
     }
 }
