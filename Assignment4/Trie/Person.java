1d943199e840d0773cdde8e6cd671604b6736726
Arpit Saxena <arpit.saxena2000@yahoo.in>
2019-09-18 20:45:43 +0530

    Completed Trie implementation

--- Assignment4/Trie/Person.java
+++ Assignment4/Trie/Person.java
@@ -1,11 +1,20 @@
 package Trie;
 
 public class Person {
+    String name;
+    String phoneNumber;
 
-    public Person(String name, String phone_number) {
+    public Person(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
     }
 
     public String getName() {
-        return "";
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "[Name: " + name + ", Phone=" + phoneNumber + "]";
     }
 }
